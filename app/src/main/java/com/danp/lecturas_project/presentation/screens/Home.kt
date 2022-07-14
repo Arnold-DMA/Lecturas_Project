c5cbb79325a27d552bcc799ed0d61e9df2ddb7ae
Karen Ccari <kccarit@unsa.edu.pe>
2022-07-13 21:35:46 -0500

    Estilo

--- app/src/main/java/com/danp/lecturas_project/presentation/screens/Home.kt
+++ app/src/main/java/com/danp/lecturas_project/presentation/screens/Home.kt
@@ -19,7 +19,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.danp.lecturas_project.R
-private val morado = Color(0x800080)
+import com.danp.lecturas_project.ui.theme.Purple200
+import com.danp.lecturas_project.ui.theme.Purple500
+import com.danp.lecturas_project.ui.theme.Purple700
+import com.danp.lecturas_project.ui.theme.cyan
 @Composable
 fun Home(navController: NavHostController) {
     Column( modifier = Modifier
@@ -28,14 +31,14 @@ fun Home(navController: NavHostController) {
         Text(
             text = "Bienvenid@ !!!",
 
-            style = TextStyle(color = Color.Black, fontSize = 42.sp, fontWeight = FontWeight.Black , fontFamily = FontFamily.Serif)
+            style = TextStyle(color = Purple700, fontSize = 42.sp, fontWeight = FontWeight.Black , fontFamily = FontFamily.Serif)
         )
         Row(modifier = Modifier
             .padding(10.dp)) {
             Text(
                 text = "Sabias que..",
 
-                style = TextStyle(color = Color.Black, fontSize = 22.sp, fontWeight = FontWeight.Black , fontFamily = FontFamily.Monospace)
+                style = TextStyle(color = Purple500, fontSize = 22.sp, fontWeight = FontWeight.Black , fontFamily = FontFamily.Monospace)
             )
             Image(painter = painterResource(id = R.drawable.pregunta), contentDescription = "pregunta", modifier = Modifier
                 .height(150.dp)
@@ -46,7 +49,7 @@ fun Home(navController: NavHostController) {
         Text(
             text = "”Los niveles de lectura nunca han sido muy altos en Perú en comparación con otros países de la región, pero hay un decrecimiento importante respecto a cómo estábamos hace dos años”",
 
-            style = TextStyle(color = Color.Black, fontSize = 12.sp, fontWeight = FontWeight.Black , fontFamily = FontFamily.Serif, textAlign = TextAlign.Center)
+            style = TextStyle(color = cyan, fontSize = 12.sp, fontWeight = FontWeight.Black , fontFamily = FontFamily.Serif, textAlign = TextAlign.Center)
         )
         Image(painter = painterResource(id = R.drawable.bajoporcentaje), contentDescription = "pregunta", modifier = Modifier
             .height(250.dp)
@@ -55,7 +58,7 @@ fun Home(navController: NavHostController) {
         Text(
             text = "Por ello te presentamos esta aplicacion que te ayudara a mejorar tu niveles de lectura y concetracion.",
 
-            style = TextStyle(color = Color.DarkGray, fontSize = 12.sp, fontWeight = FontWeight.Black , fontFamily = FontFamily.Serif, textAlign = TextAlign.Center)
+            style = TextStyle(color = Purple500, fontSize = 12.sp, fontWeight = FontWeight.Black , fontFamily = FontFamily.Serif, textAlign = TextAlign.Center)
         )
 
 
